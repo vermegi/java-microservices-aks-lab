3bf257ee1f78d34ac7c34ea360f51ff6465e2bf0
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2023-06-02 13:05:00 +0000

    WI works

--- src/spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/services/EventHubListener.java
+++ src/spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/services/EventHubListener.java
@@ -1,18 +1,18 @@
-package org.springframework.samples.petclinic.customers.services;
+// package org.springframework.samples.petclinic.customers.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.kafka.annotation.KafkaListener;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class EventHubListener {
+// @Service
+// public class EventHubListener {
 
-   private static final Logger log = LoggerFactory.getLogger(EventHubListener.class);
+//    private static final Logger log = LoggerFactory.getLogger(EventHubListener.class);
 
-   @KafkaListener(topics = "telemetry", groupId = "$Default")
-     public void receive(String in) {
-        log.info("Received message from kafka queue: {}",in);
-        System.out.println(in);
-    }
-} 
+//    @KafkaListener(topics = "telemetry", groupId = "$Default")
+//      public void receive(String in) {
+//         log.info("Received message from kafka queue: {}",in);
+//         System.out.println(in);
+//     }
+// } 
